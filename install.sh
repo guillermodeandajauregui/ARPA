fd4bae38f9eb055a3ea5861a51874cf1f789a19e
Laura-Gomez <lgomez@lcg.unam.mx>
2020-06-03 21:09:55 -0500

    bash file with installation commandos: R packages and latex

---
+++ install.sh
@@ -0,0 +1,28 @@
+
+#######################################################
+##### THIS SCRIPT WILL INSTALL ALL R DEPENDENCIES
+#######################################################
+
+
+##### VERIFY THE EXISTENCE OF COMMAND RSCRIPT
+command -v Rscript >/dev/null 2>&1 || { echo >&2 "I require Rscript but it's not installed.  Aborting."; exit 1; }
+
+
+##### INSTALLING DEPENDENCIES
+sudo Rscript -e 'install.packages("shiny")'
+sudo Rscript -e 'install.packages("shinythemes")'
+sudo Rscript -e 'install.packages("shinyFiles", repos = "https://radiant-rstats.github.io/minicran/")'
+sudo Rscript -e 'install.packages("DT")'
+sudo Rscript -e 'install.packages("tidyverse")'
+sudo Rscript -e 'install.packages("rmarkdown")'
+sudo Rscript -e 'install.packages("cowplot")'
+sudo Rscript -e 'install.packages("ggpubr")'
+sudo Rscript -e 'install.packages("kableExtra")'
+sudo Rscript -e 'install.packages("growthrates")'
+
+
+
+##### VERIFY THE EXISTENCE OF COMMAND RSCRIPT
+command -v apt-get >/dev/null 2>&1 || { echo >&2 "Latex update could not be done. apt-get required.  Aborting."; exit 1; }
+
+sudo apt-get install texlive-full
