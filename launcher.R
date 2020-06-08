505d73a3f4714161345bd12c7243ea7275d1c261
Laura-Gomez <lgomez@lcg.unam.mx>
2020-06-08 11:14:56 -0500

    Define firefox as the default browser only in linux

--- launcher.R
+++ launcher.R
@@ -1,2 +1,10 @@
-options(browser = "/usr/bin/firefox")
+
+
+###### DETERMINE FIREFOX AS THE DEFAULT BROWSER IN LINNUX
+osSystem <- Sys.info()["sysname"]
+
+if (osSystem == "Linux"){
+	options(browser = "/usr/bin/firefox")
+}
+
 options(shiny::runApp('.', launch.browser = TRUE))
