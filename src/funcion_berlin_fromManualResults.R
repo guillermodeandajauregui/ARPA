0c95c1147c92fa7b23915ad3a0c7265422efeabe
gdj <gdeanda@inmegen.edu.mx>
2020-06-18 16:00:58 -0500

    logic for FQ implemented

--- src/funcion_berlin_fromManualResults.R
+++ src/funcion_berlin_fromManualResults.R
@@ -49,9 +49,12 @@ funcion_berlin_fromManualResults <- function(input_eds,
   #define QC names 
   #this is hard code for now, until we get feedback on actual names and roles
   #used by analytics team
+  
+  #CURRENT REQUESTS OMIT DOING QC CHECK
+  
   ##############################################################################
   
-  qc_names = c("NTC", "PTC", "CRE")
+  #qc_names = c("NTC", "PTC", "CRE")
   
   
   #read eds 
@@ -64,12 +67,24 @@ funcion_berlin_fromManualResults <- function(input_eds,
   my_results <- vroom::vroom(input_txt, skip = 8, delim = "\t") 
   my_results <- janitor::clean_names(my_results)
   ###may come with some NAs, so lets drop them
+  
   my_results <- my_results %>% 
     filter(!is.na(sample_name)) %>% 
     #and let's make the cts numeric, so that we can do logics
     mutate(ct = ifelse(ct == "Undetermined", Inf, ct)) %>% 
     mutate(ct = as.numeric(ct)) 
   
+  
+  #what is a sample? 
+  #samples start with "CoV-"
+  
+  my_samples = grep(pattern = "^CoV-", x = my_results$sample_name, value = T)
+  
+  my_results <-
+  my_results %>% 
+    filter(sample_name%in%my_samples)
+  
+  #we wont carry the QC samples because FQ uses a different approach to QC
   
   #manipulate so that each row is a sample
   
@@ -78,20 +93,25 @@ funcion_berlin_fromManualResults <- function(input_eds,
     select(sample_name, target_name, ct, ct_threshold) %>% 
     pivot_wider(id_cols = sample_name,
                 names_from = target_name, 
-                values_from = ct) %>% 
-    janitor::clean_names()
+                values_from = ct) #%>% 
+    #janitor::clean_names()
   
-  #break results in samples and qc
+  test.samples = results.pivoted
   
-  test.samples <-
-    results.pivoted %>% 
-    filter(!(sample_name%in%qc_names)) #%>% 
-    #pull(sample_name) %>% .[!is.na(.)]
-  
-  qc.samples <-
-    results.pivoted %>% 
-    filter((sample_name%in%qc_names)) #%>% 
-    #pull(sample_name) %>% .[!is.na(.)]
+  # #break results in samples and qc
+  # 
+  # 
+  # 
+  # 
+  # test.samples <-
+  #   results.pivoted %>% 
+  #   filter(sample_name%in%my_samples) #%>% 
+  #   #pull(sample_name) %>% .[!is.na(.)]
+  # 
+  # qc.samples <-
+  #   results.pivoted %>% 
+  #   filter(!(sample_name%in%my_samples)) #%>% 
+  #   #pull(sample_name) %>% .[!is.na(.)]
   
   ##do results for qc
   ##############################################################################
@@ -101,35 +121,51 @@ funcion_berlin_fromManualResults <- function(input_eds,
   #standard names for genes/probes
   #acceptance status for each probe
   ##############################################################################
-  qc.results <- 
-  qc.samples %>% 
-  mutate(classification = case_when(sample_name == "NTC" & gen_e > 38 & gen_r_nasa_p > 38~ "PASS", 
-                                    sample_name == "CRE" & gen_e > 38 & gen_r_nasa_p > 38~ "PASS", 
-                                    sample_name == "PTC" & gen_e <= 38 & gen_r_nasa_p <= 38 ~ "PASS", 
-                                    TRUE ~ as.character("FAIL")
-                                    )
-         )
   
+  ####NO QC DONE
   
-  ntc.pass = (qc.results %>% filter(sample_name=="NTC") %>% pull(classification))
-  ptc.pass = (qc.results %>% filter(sample_name=="PTC") %>% pull(classification))
-  ec.pass  = (qc.results %>% filter(sample_name=="PTC") %>% pull(classification))
-  
-  qc.assess = all(sapply(list(ntc.pass, ptc.pass, ec.pass), function(i){i=="PASS"}))
-  qc.assess = ifelse(qc.assess, "PASS", "FAIL")
-  
-  qc_results <- list(qc.values = qc.results,
-                       ntc.pass = ntc.pass,
-                       ptc.pass = ptc.pass,
-                       ec.pass  = ec.pass,
-                       QC = qc.assess)
-  
+  # qc.results <- 
+  # qc.samples %>% 
+  # mutate(classification = case_when(sample_name == "NTC" & gen_e > 38 & gen_r_nasa_p > 38~ "PASS", 
+  #                                   sample_name == "CRE" & gen_e > 38 & gen_r_nasa_p > 38~ "PASS", 
+  #                                   sample_name == "PTC" & gen_e <= 38 & gen_r_nasa_p <= 38 ~ "PASS", 
+  #                                   TRUE ~ as.character("FAIL")
+  #                                   )
+  #        )
+  # 
+  # 
+  # ntc.pass = (qc.results %>% filter(sample_name=="NTC") %>% pull(classification))
+  # ptc.pass = (qc.results %>% filter(sample_name=="PTC") %>% pull(classification))
+  # ec.pass  = (qc.results %>% filter(sample_name=="PTC") %>% pull(classification))
+  # 
+  # qc.assess = all(sapply(list(ntc.pass, ptc.pass, ec.pass), function(i){i=="PASS"}))
+  # qc.assess = ifelse(qc.assess, "PASS", "FAIL")
+  # 
+  # qc_results <- list(qc.values = qc.results,
+  #                      ntc.pass = ntc.pass,
+  #                      ptc.pass = ptc.pass,
+  #                      ec.pass  = ec.pass,
+  #                      QC = qc.assess)
+  # 
+  fq_message = "FQ does not asks for QC"
+  qc_results <- list(qc.values = fq_message,
+                     ntc.pass = fq_message,
+                     ptc.pass = fq_message,
+                     ec.pass  = fq_message,
+                     QC = fq_message)
   
   #do results for samples 
   
   test.results <- 
     test.samples %>% 
-    mutate(classification = ifelse(gen_e <= 38, "positive", "negative" ))
+    #mutate(classification = ifelse(gen_e <= 38, "positive", "negative" ))
+    mutate(classification = case_when((`Gen E` <= 38 & `Gen Rd.Rp` <= 38 &  `Gen Rp` <= 38) ~ "positivo",
+                                      (`Gen E` > 38 & `Gen Rd.Rp` > 38 & `Gen Rp` <= 38) ~ "negativo", 
+                                      `Gen Rp` > 38 ~ "invalido", 
+                                      TRUE ~ "indederminado"
+                                      
+                                      )
+           )
   
   ################################################################################
   #Plot preparation
