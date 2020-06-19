0062c02d064dc0e074c5c9724f82a8700bac8f1c
hachepunto <hachepunto@gmail.com>
2020-06-19 10:18:47 -0500

    comment booklet report function

--- src/link_reports.R
+++ src/link_reports.R
@@ -12,21 +12,21 @@ individual_reports <-function(single_plots, qc_results, input_eds, output ){
 }
 
 
-booklet_report <- function(test_results, qc_results, input_eds, output){
+# booklet_report <- function(test_results, qc_results, input_eds, output){
   
- ################################################################################ 
- #Get the plate name
- ################################################################################
+#  ################################################################################ 
+#  #Get the plate name
+#  ################################################################################
 
- plate <- stringr::str_remove(string = basename(input_eds), pattern = ".eds")
+#  plate <- stringr::str_remove(string = basename(input_eds), pattern = ".eds")
 
- ################################################################################
- #Write plate Booklet
- ################################################################################
+#  ################################################################################
+#  #Write plate Booklet
+#  ################################################################################
 
- test_diagnosis <- cbind(plate, test_results)
+#  test_diagnosis <- cbind(plate, test_results)
 
- plateBooklet(results = test_diagnosis,
-             qc_results = qc_results,
-             outdir = output)
-}
+#  plateBooklet(results = test_diagnosis,
+#              qc_results = qc_results,
+#              outdir = output)
+# }
