33c7c4df3be500430d17ac5e0dee78ed52a9ef3f
gdj <gdeanda@inmegen.edu.mx>
2021-02-04 12:58:25 -0600

    wrapped all funcion_berlin assignations in try, result assignations in tryCatch; return error message (chr) on error

--- src/funcion_berlin.R
+++ src/funcion_berlin.R
@@ -19,7 +19,7 @@ funcion_berlin <- function(input_eds,
   
   ### check eds has results
   
-  has_results <- CheckResultsEDS(eds = input_eds)
+  has_results <- try(CheckResultsEDS(eds = input_eds))
   if(has_results == FALSE){
     return("no_results")
   }
@@ -35,12 +35,12 @@ funcion_berlin <- function(input_eds,
   #define probes
   ########
   
-  berlin_probes <- c("Gen E", "Gen RNasaP")
-  names(berlin_probes) <- berlin_probes
+  berlin_probes <- try(c("Gen E", "Gen RNasaP"))
+  names(berlin_probes) <- try(berlin_probes)
   
   ### check that all samples have all probes
   
-  has_allProbes <- CheckProbesEDS(eds = input_eds, my_probes = berlin_probes)
+  has_allProbes <- try(CheckProbesEDS(eds = input_eds, my_probes = berlin_probes))
   if(has_allProbes == FALSE){
     warning("some_samples_are_missing_probes")
   }
@@ -53,64 +53,76 @@ funcion_berlin <- function(input_eds,
   
   qc_names = c("NTC", "PTC", "CRE")
   
-  has_all_qc <- CheckControlsEDS(eds = input_eds, controls = qc_names)
+  has_all_qc <- try(CheckControlsEDS(eds = input_eds, controls = qc_names))
   if(has_all_qc==FALSE){
     warning("do not have exactly 3 controls")
   }
   #read eds 
   
-  my_deltaRN <- tidy_deltaRN(input_eds) #read deltaRN from EDS file 
+  my_deltaRN <- try(tidy_deltaRN(input_eds)) #read deltaRN from EDS file 
   
   # analyze curves, determine sample ct
-  test.samples <- test.plate(tdrn = my_deltaRN, probes = berlin_probes)
-  names(test.samples) <- c("sample_name", "gen_e", "gen_r_nasa_p")
+  test.samples <- try(test.plate(tdrn = my_deltaRN, probes = berlin_probes))
+  names(test.samples) <- try(c("sample_name", "gen_e", "gen_r_nasa_p"))
   
   #curve plot
   plate_curves <- 
-    pivot_deltaRN(my_deltaRN) %>% 
-    plot_deltaRN.long()
+    try(pivot_deltaRN(my_deltaRN) %>% 
+    plot_deltaRN.long())
   
   #qc results
-  qc_results <- plate_qc.berlin(tdrn = my_deltaRN, 
-                                all_probes = berlin_probes)
+  qc_results <-
+    tryCatch( expr = {plate_qc.berlin(tdrn = my_deltaRN, 
+                                all_probes = berlin_probes)},
+              error = function(e){return(as.character(e))}
+              
+  )
   
 
  #do results for samples 
   
   test.results <- 
-    test.samples %>% 
-    mutate(classification = case_when(gen_e <= 40 & gen_r_nasa_p <= 40 ~ "Positivo",
-                                      gen_e > 40 & gen_r_nasa_p <= 40 ~ "Negativo",
-                                      gen_r_nasa_p > 40 ~ "Repetir"
-    )) 
+    tryCatch(expr = {test.samples %>% 
+        mutate(classification = case_when(gen_e <= 40 & gen_r_nasa_p <= 40 ~ "Positivo",
+                                          gen_e > 40 & gen_r_nasa_p <= 40 ~ "Negativo",
+                                          gen_r_nasa_p > 40 ~ "Repetir"
+        ))},
+             error = function(e){return(as.character(e))}
+             )
+     
 
   #############
   #list of single figure plots
   #############
   
-  all_samples <- c(test.results$sample_name %>% unique, qc_names)
-  names(all_samples) <- c(all_samples)
+  all_samples <- try(c(test.results$sample_name %>% unique, qc_names))
+  names(all_samples) <- try(c(all_samples))
   
-  single_plots <- lapply(all_samples, FUN = function(i){
-    sample_curve.berlin.manual(tdrn = my_deltaRN, 
-                               sample_id = i, 
-                               probes = berlin_probes
-    )
-  })
+  single_plots <- 
+    tryCatch(expr = {lapply(all_samples, FUN = function(i){
+      sample_curve.berlin.manual(tdrn = my_deltaRN, 
+                                 sample_id = i, 
+                                 probes = berlin_probes
+      )
+    })},
+             error = function(e){return(as.character(e))}
+             )
+    
+    
   
-  make_reports(plot_list = single_plots, 
+  try(make_reports(plot_list = single_plots, 
               result_table = qc_results$qc.values[,1:3], 
               input = input_eds,
               outdir = output, 
               qc_results = qc_results$QC,
-             qc = F)
+             qc = F))
   
-  make_reports(plot_list = single_plots, 
+  try(make_reports(plot_list = single_plots, 
               result_table = qc_results$qc.values[,1:3], 
               input = input_eds,
               outdir = output, 
               qc_results = qc_results$QC,
-              qc = T)
+              qc = T))
   
   ################################################################################
   #Create list output
