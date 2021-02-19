4867243c328eb3da374a6e0c9041118c7f961f86
Laura-Gomez <lgomez@lcg.unam.mx>
2021-02-18 18:46:24 -0600

    Logo only ARPA

--- app.R
+++ app.R
@@ -36,7 +36,7 @@ ui <- fluidPage(
   ),
   
   titlePanel( div(column(width = 6, h1("ARPA - Automatic analysis of RT-PCR results")), 
-                  column(width = 4, tags$img(src = "images/conjunta.jpeg"))),
+                  column(width = 4, tags$img(src = "images/arpa.jpeg"))),
               windowTitle="rt-PCR-analysis"),
   
   hr(),
